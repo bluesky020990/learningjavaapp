0d90791d01c49ef5364d5f319dd201c5d71ef1c8
Khanh Tran <bluesky020990@gmail.com>
2019-03-12 22:55:38 +0700

    Lesson 04. Router.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,20 +1,36 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from '../../data-model/hero';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from '../../services/hero.service';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
+
 export class HeroComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
 
-  constructor() { }
-
-  ngOnInit() {
-
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) {
   }
 
+  ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -16,4 +16,8 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }

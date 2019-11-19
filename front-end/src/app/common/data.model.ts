a0126b57898faa43e60e517e0d17066462fe0534
Khanh Tran <bluesky020990@gmail.com>
2019-11-19 23:55:00 +0700

    Revise Page structure.

--- front-end/src/app/common/data.model.ts
+++ front-end/src/app/common/data.model.ts
@@ -2,26 +2,29 @@ export interface LessonMappingI{
   path: string;
   name: string;
   displayOrder;
+  children;
 }
 
 export class LessonMapping implements LessonMappingI{
   path: string;
   name: string;
   displayOrder;
+  children: LessonMapping[];
 
-  constructor(path: string, name: string, displayOrder) {
+  constructor(path: string, name: string, displayOrder, children: LessonMapping[]) {
     this.path = path;
     this.name = name;
     this.displayOrder = displayOrder;
+    this.children = children;
   }
+
 }
 
 export class LessonCategory extends LessonMapping{
   category: string;
 
-
-  constructor(path: string, name: string, displayOrder, category: string) {
-    super(path, name, displayOrder);
+  constructor(path: string, name: string, displayOrder, category: string, children: LessonMapping[]) {
+    super(path, name, displayOrder, children);
     this.category = category;
   }
 }
